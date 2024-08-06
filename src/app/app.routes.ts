ae9cf7fe6ae15d227b480181a800c8bc5368bbf1
Rakesh Kumar Kuna <kunarakeshkumar@gmail.com>
2024-08-06 16:03:44 +0530

    about page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-    {path:"cart",component:CartComponent}
+    {path:"cart",component:CartComponent},
+    {path:"about",component:AboutComponent}
 ];
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,23 +1,25 @@
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ApidataService } from '../Services/apidata.service';
-import { NgFor } from '@angular/common';
+import { CurrencyPipe, NgFor } from '@angular/common';
 import { SlicePipe } from '@angular/common';
-
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [NgFor,SlicePipe],
+  imports: [NgFor,SlicePipe,CurrencyPipe],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent implements OnInit  {
   public cartItems:any[] = [];
   public count:number=1;
+  public totalCount:number=0;
 constructor(private http:HttpClient,private api:ApidataService){}
   ngOnInit(): void {
-    this.api.getcartItems().subscribe((data)=>{this.cartItems=data});
-
+    this.api.getcartItems().subscribe((data)=>{
+      this.cartItems=data
+     
+    });
   }
 
 }
